c4a24429687a54c4dc190c608e00d000d9070ef1
ZeriX911 <ZeriX911@gmail.com>
2022-03-15 15:28:04 +0100

    maps

---
+++ scripts/maps.js
@@ -0,0 +1,24 @@
+const dcs = document.getElementsByClassName("menu-chooser");
+
+function clickedBR(params) {
+    removeSelector(0);
+    params.classList.add('activeIcon');
+}
+
+function clickedAR(params) {
+    removeSelector(1);
+    params.classList.add('activeIcon');
+}
+
+function clickedSPEC(params) {
+    removeSelector(2);
+    params.classList.add('activeIcon');
+}
+
+function removeSelector(index) {
+    dcs[index].childNodes.forEach(element => {
+        if (element.hasChildNodes()) {
+            element.classList.remove('activeIcon')
+        }
+    });
+}
