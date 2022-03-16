0718cbcd84e8a205792278b1cebc12a60c970e37
ZeriX911 <ZeriX911@gmail.com>
2022-03-16 18:58:05 +0100

    just for the memes

--- scripts/maps.js
+++ scripts/maps.js
@@ -4,17 +4,25 @@ function clickedBR(params) {
     removeSelector(0);
     params.classList.add('activeIcon');
     let content = document.getElementById("br-content");
-
+    cleanClassList(content);
+    content.classList.add("br-" + params.id);
 }
 
 function clickedAR(params) {
     removeSelector(1);
     params.classList.add('activeIcon');
+    let content = document.getElementById("ar-content");
+    cleanClassList(content);
+    content.classList.add("ar-" + params.id);
 }
 
 function clickedSPEC(params) {
     removeSelector(2);
     params.classList.add('activeIcon');
+    let content = document.getElementById("spec-content");
+    cleanClassList(content);
+    content.classList.add("sp-" + params.id);
+    console.log(params.id)
 }
 
 function removeSelector(index) {
@@ -22,6 +30,14 @@ function removeSelector(index) {
         if (element.hasChildNodes()) {
             element.classList.remove('activeIcon')
 
+        }
+    });
+}
+
+function cleanClassList(Element) {
+    Element.classList.forEach(elem => {
+        if (elem != "content") {
+            Element.classList.remove(elem);
         }
     });
 }
