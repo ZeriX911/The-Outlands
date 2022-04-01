b43deee5374f5660f21258e5c77322ee8e3cfc39
rozsapatrik <rozsapatrik007@gmail.com>
2022-04-01 10:18:27 +0200

    reglogindone

--- pages/createpost.php
+++ pages/createpost.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false) {
+  header("Location: /pages/index.php");
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
