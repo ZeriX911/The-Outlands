c265281ee35c5bf439d847e18a45e9498e0e621a
rozsapatrik <rozsapatrik007@gmail.com>
2022-04-11 12:13:03 +0200

    yeet

--- pages/listprofiles.php
+++ pages/listprofiles.php
@@ -26,17 +26,29 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false) {
     <div id="page-container">
         <div id="content-wrap">
         <?php include('header.php');?>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <form>
                     <main id="Container">
                     <fieldset>
                         <h1 id="logintitle">Other Profiles</h1>
                         <div id="otherprofiles-container">
-                        <select name="otherprofiles" id="otherprofiles">
-                            <option value="sample">sample</option>
-                        </select>
-                        </div>
-                        <div id="send-container">
-                            <input type="submit" value="LOGIN">
+                        
+                        <?php
+                        require_once('connect.php');
+
+                        $sql = "SELECT username FROM users";
+                        $result = $connect -> query($sql);
+                        $users = array();
+
+                        if($result)
+                        {
+                            while($row = $result -> fetch_array())
+                            {
+                                echo "<span class='users'><a href='otherprofile.php?gid={$row[0]}'>{$row[1]}</a></span></br>";
+                                array_push($users, $row[1]);
+                            }
+                        }
+                        $connect -> close();
+                        ?>
                         </div>
                       </fieldset>
                     </main>
